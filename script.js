f478765f812a95d063466bfe539f9a0f8db38359
Marco Nuñez <marco.nunez@aumenta.mx>
2024-02-08 21:01:44 -0600

    first commit

---
+++ script.js
@@ -0,0 +1,47 @@
+var height = 100;
+var width = 100;
+
+function showMessage(response) {
+  if (response === "No") {
+    const noButton = document.getElementById("no-button");
+    const yesButton = document.getElementById("yesButton");
+    const container = document.querySelector(".container");
+    const maxWidth = window.innerWidth - noButton.offsetWidth;
+    const maxHeight = window.innerHeight - noButton.offsetHeight;
+
+    noButton.style.position = "absolute";
+
+    document.getElementsByClassName("image")[0].src = "images/gun.gif";
+
+    const randomX = Math.max(0, Math.floor(Math.random() * maxWidth));
+    const randomY = Math.max(0, Math.floor(Math.random() * maxHeight));
+
+    noButton.style.left = randomX + "px";
+    noButton.style.top = randomY + "px";
+
+    yesButton.style.width  = width + "px";
+    yesButton.style.height  = height + "px";
+
+    height +=50;
+    width += 50;
+
+    document.getElementById("question").textContent =
+      "El botón de 'no' es solo de adorno";
+    document.getElementById("name").style.display = "none";
+
+  }
+
+  if (response === "Yes") {
+
+    document.getElementById("name").remove();
+    document.getElementById("no-button").remove();
+
+    const yesMessage = document.getElementById("question");
+    yesMessage.textContent = "YEIIII te veo el 14 hermosa 😘";
+    yesMessage.style.display = "block";
+    yesMessage.style.fontStyle = "normal";
+    document.getElementsByClassName("image")[0].src = "images/dance.gif";
+
+    document.getElementById("yesButton").remove();
+  }
+}
